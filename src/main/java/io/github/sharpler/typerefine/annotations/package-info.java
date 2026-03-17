8c042e1071af32a04126693b686c6ca9bae779b7
sharpler <sharplermc@gmail.com>
2026-03-17 21:01:03 +0400

    prepare to publish #7

--- src/main/java/io/github/sharpler/typerefine/annotations/package-info.java
+++ src/main/java/io/github/sharpler/typerefine/annotations/package-info.java
@@ -1,2 +1,4 @@
-@org.jspecify.annotations.NullMarked
+@NullMarked
 package io.github.sharpler.typerefine.annotations;
+
+import org.jspecify.annotations.NullMarked;
--- src/main/java/io/github/sharpler/typerefine/processor/package-info.java
+++ src/main/java/io/github/sharpler/typerefine/processor/package-info.java
@@ -1,2 +1,4 @@
-@org.jspecify.annotations.NullMarked
+@NullMarked
 package io.github.sharpler.typerefine.processor;
+
+import org.jspecify.annotations.NullMarked;
